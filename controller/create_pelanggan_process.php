9441d9e73b81a1e4d324b95478d4101999691de9
michaelollifarel05 <78914548+michaelollifarel05@users.noreply.github.com>
2023-05-06 17:55:48 +0700

    Update create_pelanggan_process.php

--- controller/create_pelanggan_process.php
+++ controller/create_pelanggan_process.php
@@ -9,7 +9,7 @@ $tanggal_gabung = $_POST['tanggal_gabung'];
 
 $sql = "INSERT INTO pelanggan (idPenjualan, NamaPelanggan, NoHp, TanggalGabung) VALUES ($id_penjualan, '$nama_pelanggan', '$no_hp', '$tanggal_gabung')";
 if (mysqli_query($conn, $sql)) {
-    echo "Data berhasil ditambahkan";
+    header("Location: controller.php");
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
