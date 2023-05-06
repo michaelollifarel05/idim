72218ce9833573180b456fd32e90f2d6936777bc
nandyhadi <69504957+nandyhadi@users.noreply.github.com>
2023-05-06 17:35:08 +0700

    Add files via upload

---
+++ controller/create_pelanggan_process.php
@@ -0,0 +1,16 @@
+<?php
+include 'koneksi.php';
+
+$nama_pelanggan = $_POST['nama_pelanggan'];
+$no_hp = $_POST['no_hp'];
+$tanggal_gabung = $_POST['tanggal_gabung'];
+
+$sql = "INSERT INTO pelanggan (NamaPelanggan, NoHp, TanggalGabung) VALUES ('$nama_pelanggan', '$no_hp', '$tanggal_gabung')";
+if (mysqli_query($conn, $sql)) {
+    echo "Data berhasil ditambahkan";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
