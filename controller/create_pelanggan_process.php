b77aee3a0436772609e6b6fb0c805f102378fa02
nandyhadi <69504957+nandyhadi@users.noreply.github.com>
2023-05-06 17:52:25 +0700

    Update create_pelanggan_process.php

--- controller/create_pelanggan_process.php
+++ controller/create_pelanggan_process.php
@@ -2,11 +2,12 @@
 include '../koneksi.php';
 $db = new Database();
 $conn = $db->connect();
+$id_Penjualan = $_POST['id_Penjualan'];
 $nama_pelanggan = $_POST['nama_pelanggan'];
 $no_hp = $_POST['no_hp'];
 $tanggal_gabung = $_POST['tanggal_gabung'];
 
-$sql = "INSERT INTO pelanggan (NamaPelanggan, NoHp, TanggalGabung) VALUES ('$nama_pelanggan', '$no_hp', '$tanggal_gabung')";
+$sql = "INSERT INTO pelanggan (idPenjualan, NamaPelanggan, NoHp, TanggalGabung) VALUES ($id_penjualan, '$nama_pelanggan', '$no_hp', '$tanggal_gabung')";
 if (mysqli_query($conn, $sql)) {
     echo "Data berhasil ditambahkan";
 } else {
